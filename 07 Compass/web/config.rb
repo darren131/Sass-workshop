14b3a65ac73f13b5fe7a2ef840592d8dbed8b16c
Darren Wood <darren@dontcom.com>
2013-03-25 22:17:27 +1300

    various little tweaks
    - rearranged folders
    - added some more ASCII
    - more awesomer

--- 07 Compass/web/config.rb
+++ 07 Compass/web/config.rb
@@ -3,7 +3,7 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "assets/css"
+css_dir = "assets/stylesheets"
 sass_dir = "assets/sass"
 images_dir = "assets/images"
 javascripts_dir = "assets/scripts"
