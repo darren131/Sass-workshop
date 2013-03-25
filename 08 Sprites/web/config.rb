b1e7092afe1aff48e5afdda4a906dee0c87eb555
Darren Wood <darren@dontcom.com>
2013-03-25 17:27:50 +1300

    sprite generation

---
+++ 08 Sprites/web/config.rb
@@ -0,0 +1,16 @@
+# Require any additional compass plugins here.
+# require "plugin"
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "assets/css"
+sass_dir = "assets/sass"
+images_dir = "assets/images"
+javascripts_dir = "assets/scripts"
+fonts_dir = "assets/fonts"
+
+output_style = :expanded
+environment = :development
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
