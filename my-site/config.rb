6e211be5ad5037d2cc8ed7ac985ea6a65d190698
Darren Wood <darren@dontcom.com>
2013-04-07 14:17:02 +1200

    added some new files

---
+++ my-site/config.rb
@@ -0,0 +1,16 @@
+# Require any additional compass plugins here.
+# require "susy"
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "stylesheets"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "scripts"
+fonts_dir = "fonts"
+
+output_style = :expanded
+environment = :development
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
